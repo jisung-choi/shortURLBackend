f9df7e644b27473e80a11096953314fd605501cd
James Jisung Choi <70044510+jisung-choi@users.noreply.github.com>
2023-07-12 16:53:33 -0700

    Initialized Project

---
+++ src/test/java/myProject/shortURL/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package myProject.shortURL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
